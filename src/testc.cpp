81d1b3fcfebb19445f337e926384888cba761208
Asmita Roy <asmita112358@tamu.edu>
2019-11-20 14:15:08 -0600

    Inserted the fast covariance code

--- src/testc.cpp
+++ src/testc.cpp
@@ -1,27 +1,43 @@
+// [[Rcpp::depends(RcppArmadillo)]]
+
+#include <RcppArmadilloExtensions/sample.h>
+#include <RcppArmadillo.h>
 #include <Rcpp.h>
+
 using namespace Rcpp;
-
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
-
+using namespace arma;
 // [[Rcpp::export]]
-NumericVector timesTwo(NumericVector x) {
-  return x * 2;
+
+
+
+double d_cov(  arma::vec x, arma::vec y, double N)
+  {
+  int n = x.size();
+  int Upper=100;
+  float c=pow(3.1415,2)/2;
+  
+  arma::colvec  u=linspace(1,100,100);
+  
+  arma::colvec v = 1/c*1/pow(u-0.5,2);
+  
+  arma::colvec p = v/sum(v);
+  arma::colvec k = Rcpp::RcppArmadillo::sample(u,N,TRUE,p);
+  arma::colvec kv= (k-0.5)*3.1415;
+  arma::mat U= x*trans(kv);
+  arma::mat V= y*trans(kv);
+  arma::mat phix= sqrt(2/N)*sin(U);
+  arma::mat phiy= sqrt(2/N)*sin(V);
+  arma::mat Kx= phix*trans(phix);
+  arma::mat Ky= phiy*trans(phiy);
+  Kx.diag(0).fill(0);
+  Ky.diag(0).fill(0);
+  arma::mat rx=sum(Kx);
+  arma::mat ry=sum(Ky);
+  double t3=2*as_scalar(rx*trans(ry))/(n-2);
+  double t1=as_scalar(trace(Kx*Ky));
+  double t2=accu(rx)*accu(ry)/((n-1)*(n-2));
+  double d=(t1+t2-t3)/((n-3)*n);
+  return d;
+  //return List::create( Named("Beta") =t3);
+  
 }
-
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
-// run after the compilation.
-//
-
-/*** R
-timesTwo(42)
-*/
